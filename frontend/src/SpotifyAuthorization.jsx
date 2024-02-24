89dd2f92da7ce76711a16e14b9aafb7553e1a82a
Dylan <dylanpwgo@gmail.com>
2024-02-23 16:24:35 -0800

    Added access token acquisition

--- frontend/src/SpotifyAuthorization.jsx
+++ frontend/src/SpotifyAuthorization.jsx
@@ -1,11 +1,14 @@
 // Authorization code
+const clientId = '6fc5eda025b8463ea9bd776424fec51d'
+const redirectUri = 'http://localhost:3001';
+
 const generateRandomString = (length) => {
     const possible = 'QwMLGUV0hZGO7vGM3vqzb25IQTbfH6gbKQxMhMFze0k';
     const values = crypto.getRandomValues(new Uint8Array(length));
     return values.reduce((acc, x) => acc + possible[x % possible.length], "");
   }
   
-  const codeVerifier  = generateRandomString(64);
+  const codeVerifier = generateRandomString(64);
   
   const sha256 = async (plain) => {
     const encoder = new TextEncoder()
@@ -19,11 +22,34 @@ const generateRandomString = (length) => {
       .replace(/\+/g, '-')
       .replace(/\//g, '_');
   }
+
+  async function getToken(code) {
+    let codeVerifier = sessionStorage.getItem('code_verifier');
+
+    const payload = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
+      }),
+    }
   
-  async function redirectToSpotifyAuthorize() {
-    const clientId = '6fc5eda025b8463ea9bd776424fec51d'
-    const redirectUri = 'http://localhost:3001';
+    const tokenEndpoint = "https://accounts.spotify.com/api/token";
+
+    const body = await fetch(tokenEndpoint, payload);
+    const response = await body.json();
   
+    sessionStorage.setItem('access_token', response.access_token);
+    console.log("hi", sessionStorage.getItem('access_token'));
+  }
+  
+  async function redirectToSpotifyAuthorize() {  
     const scope = 'user-read-private user-read-email';
     const authUrl = new URL("https://accounts.spotify.com/authorize")
   
@@ -43,20 +69,27 @@ const generateRandomString = (length) => {
     
     authUrl.search = new URLSearchParams(params).toString();
     window.location.href = authUrl.toString();
-  
+  }
+
+const SpotifyAuthorization = () => {
     const urlParams = new URLSearchParams(window.location.search);
     let code = urlParams.get('code');
     
     // If user does not accept request or error occurs
     let error = urlParams.get('error');
     let state = urlParams.get('state');
-  }
 
-const SpotifyAuthorization = () => {
-    let message = "Welcome!"
+    if (code) {
+        getToken(code);
+    }
+    // Error Handling
+    let message = "";
     if (sessionStorage.getItem('access_token') === null) {
         message = "Login to spotify to use the app";
+    } else {
+        message = "Logged in";
     }
+
     return (
         <div>
             <p>{message}</p>
@@ -66,3 +99,5 @@ const SpotifyAuthorization = () => {
 }
 
 export default SpotifyAuthorization
+
+// TODO: Use react states to accurately show when has access token
