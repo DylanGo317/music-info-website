707e4496cfc93b22ddb5905fe1457b8f3276807c
Dylan <dylanpwgo@gmail.com>
2024-02-25 18:57:27 -0800

    Fixed tab spacing

--- frontend/src/SpotifyAuthorization.jsx
+++ frontend/src/SpotifyAuthorization.jsx
@@ -51,57 +51,57 @@ const SpotifyAuthorization = () => {
 
   const [hasToken, setHasToken] = useState(false);
 
-    async function getToken(code) {
-      let codeVerifier = sessionStorage.getItem('code_verifier');
+  async function getToken(code) {
+    let codeVerifier = sessionStorage.getItem('code_verifier');
 
-      const payload = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-          client_id: clientId,
-          grant_type: 'authorization_code',
-          code,
-          redirect_uri: redirectUri,
-          code_verifier: codeVerifier,
-        }),
-      }
-    
-      const tokenEndpoint = "https://accounts.spotify.com/api/token";
-
-      const body = await fetch(tokenEndpoint, payload);
-      const response = await body.json();
-    
-      sessionStorage.setItem('access_token', response.access_token);
-      setHasToken(true);
-      console.log("token:", sessionStorage.getItem('access_token'));
+    const payload = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
+      }),
     }
+  
+    const tokenEndpoint = "https://accounts.spotify.com/api/token";
 
-    const urlParams = new URLSearchParams(window.location.search);
-    let code = urlParams.get('code');
-    
-    // If user does not accept request or error occurs
-    let error = urlParams.get('error');
-    let state = urlParams.get('state');
+    const body = await fetch(tokenEndpoint, payload);
+    const response = await body.json();
+  
+    sessionStorage.setItem('access_token', response.access_token);
+    setHasToken(true);
+    console.log("token:", sessionStorage.getItem('access_token'));
+  }
 
-    if (code && !hasToken) {
-        getToken(code);
-    }
-    // Error Handling
-    let message = "";
-    if (!hasToken) {
-        message = "Login to spotify to use the app";
-    } else {
-        message = "Logged in";
-    }
+  const urlParams = new URLSearchParams(window.location.search);
+  let code = urlParams.get('code');
+  
+  // If user does not accept request or error occurs
+  let error = urlParams.get('error');
+  let state = urlParams.get('state');
 
-    return (
-        <div>
-            <p>{message}</p>
-            <button onClick={redirectToSpotifyAuthorize}>Spotify Login</button>
-        </div>
-    )
+  if (code && !hasToken) {
+      getToken(code);
+  }
+  // Error Handling
+  let message = "";
+  if (!hasToken) {
+      message = "Login to spotify to use the app";
+  } else {
+      message = "Logged in";
+  }
+
+  return (
+      <div>
+          <p>{message}</p>
+          <button onClick={redirectToSpotifyAuthorize}>Spotify Login</button>
+      </div>
+  )
 }
 
 export default SpotifyAuthorization
